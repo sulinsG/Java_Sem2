fdf05c2a7145b5cd0f73cd2eb220384749ba0df7
Gatis <gatis.sulins@gmail.com>
2022-03-15 12:29:22 +0200

    new class added

---
+++ src/models/Course.java
@@ -0,0 +1,15 @@
+package models;
+
+
+public class Course {
+    //1. mainīgie
+    private int id;
+    private int idCounter;
+    private String title;
+    private byte creditPoints;
+    private Professor professor;
+
+    
+
+
+}
