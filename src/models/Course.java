19c058e088aa5c28fe0939fc772cc2527e29baee
Gatis <gatis.sulins@gmail.com>
2022-03-15 12:39:15 +0200

    new class get and set functions

--- src/models/Course.java
+++ src/models/Course.java
@@ -4,12 +4,68 @@ package models;
 public class Course {
     //1. mainīgie
     private int id;
-    private int idCounter;
+    private static int idCounter = 1000;
     private String title;
     private byte creditPoints;
     private Professor professor;
 
-    
+    //2.1. get funkcijas
 
 
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public byte getCreditPoints() {
+        return creditPoints;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    //2.2 set funkcijas
+    private void setId()
+    {
+        this.id = idCounter++;
+    }
+    public void setTitle(String title) {
+        if(title != null && title.isEmpty() && title.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[\\s\\d a-zēūīāšģķļņčžA-ZĀĒŪĪĻĶĢŠŽČŅ]+"))
+        {
+            this.title = title;
+        }
+        else
+            this.title = "unknown";
+    }
+
+    public void setCreditPoints(byte creditPoints) {
+        if(creditPoints > 0 && creditPoints <= 20)
+        {
+            this.creditPoints = creditPoints;
+        }
+        else
+        {
+            this.creditPoints = 2;
+        }
+    }
+
+    public void setProfessor(Professor professor) {
+        if (professor != null &&)
+        {
+            this.professor = professor;
+        }
+        else
+        {
+            this.professor = new Professor();
+        }
+
+
+    //3. konstruktori
+
+
+    //4. toString funkcija
 }
