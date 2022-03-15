82643e7ad523d2cfcdbb7c2b839238026b8be97e
Gatis <gatis.sulins@gmail.com>
2022-03-15 12:52:47 +0200

    new class student

---
+++ src/models/Student.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Student {
+    //1. mainīgie
+    private int id;
+    private static int idCounter = 10000;
+    private String name;
+    private String surname;
+
+    //2.1 get funckijas
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+
+    //2.2 set funkcijas
+    
+
+    //3. konstruktori
+
+    //4. toString funkcija
+
+
+
+
+}
